a814f5d63209811b43fb06b3147a1bba1ddcde09
Theo Vanderkooy <29787751+TheoVanderkooy@users.noreply.github.com>
2020-04-20 19:24:33 -0700

    remove extra newlines in main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,15 +4,15 @@ use sudoku::*;
 fn main() -> Result<(), SudokuError> {
 
     // debug printing
-    // println!("{}\n\n", Board::empty());
+    // println!("{}\n", Board::empty());
 
     let mut board = Board::from_sparse(&vec![(0, 0, 1), (2, 3, 2), (8, 8, 3)])?;
-    // println!("{}\n\n", board);
+    // println!("{}\n", board);
 
     board[1][1].set(4)?;
-    println!("{}\n\n", board);
+    println!("{}\n", board);
     board[1][1].set(0)?;
-    println!("\n\n{}", board);
+    println!("{}\n", board);
 
 
     Ok(())
